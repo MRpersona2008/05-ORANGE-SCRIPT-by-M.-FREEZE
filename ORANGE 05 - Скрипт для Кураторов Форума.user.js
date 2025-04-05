e3661acfa815cf0edf1151e32728930de74dfbd5
MRpersona2008 <nikitasiplyj3@gmail.com>
2025-04-05 23:23:28 +0300

    Update ORANGE 05 - Скрипт для Кураторов Форума.user.js

--- ORANGE 05 - Скрипт для Кураторов Форума.user.js
+++ ORANGE 05 - Скрипт для Кураторов Форума.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name ORANGE 05 | Скрипт для Кураторов Форума
 // @namespace https://forum.blackrussia.online
-// @version 1.3
+// @version 1.4
 // @author by M.Freeze
 // @updateversion Создан 22 марта 
 // @match https://forum.blackrussia.online/threads/*
@@ -9,8 +9,8 @@
 // @description none
 // @license MIT
 // @icon https://forum.blackrussia.online/account/avatar
-// @downloadURL https://update.greasyfork.org/scripts/527008/ORANGE%2005%20%7C%20%D0%A1%D0%BA%D1%80%D0%B8%D0%BF%D1%82%20%D0%B4%D0%BB%D1%8F%20%D0%9A%D1%83%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BE%D0%B2%20%D0%A4%D0%BE%D1%80%D1%83%D0%BC%D0%B0.user.js
-// @updateURL https://update.greasyfork.org/scripts/527008/ORANGE%2005%20%7C%20%D0%A1%D0%BA%D1%80%D0%B8%D0%BF%D1%82%20%D0%B4%D0%BB%D1%8F%20%D0%9A%D1%83%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BE%D0%B2%20%D0%A4%D0%BE%D1%80%D1%83%D0%BC%D0%B0.meta.js
+// @downloadURL 
+// @updateURL 
 // ==/UserScript==
 
 (function () {
