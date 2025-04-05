6426e208d90f0a10b60e2e5b8f94db76a5054f0b
MRpersona2008 <nikitasiplyj3@gmail.com>
2025-04-05 23:26:12 +0300

    ORANGE 05 - Скрипт для Кураторов Форума.user.js

--- ORANGE 05 - Скрипт для Кураторов Форума.user.js
+++ ORANGE 05 - Скрипт для Кураторов Форума.user.js
@@ -9,8 +9,8 @@
 // @description none
 // @license MIT
 // @icon https://forum.blackrussia.online/account/avatar
-// @downloadURL 
-// @updateURL 
+// @downloadURL https://github.com/MRpersona2008/05-ORANGE-SCRIPT-by-M.-FREEZE/blob/e3661acfa815cf0edf1151e32728930de74dfbd5/ORANGE%2005%20-%20%D0%A1%D0%BA%D1%80%D0%B8%D0%BF%D1%82%20%D0%B4%D0%BB%D1%8F%20%D0%9A%D1%83%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BE%D0%B2%20%D0%A4%D0%BE%D1%80%D1%83%D0%BC%D0%B0.user.js
+// @updateURL https://github.com/MRpersona2008/05-ORANGE-SCRIPT-by-M.-FREEZE/blob/e3661acfa815cf0edf1151e32728930de74dfbd5/ORANGE%2005%20-%20%D0%A1%D0%BA%D1%80%D0%B8%D0%BF%D1%82%20%D0%B4%D0%BB%D1%8F%20%D0%9A%D1%83%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BE%D0%B2%20%D0%A4%D0%BE%D1%80%D1%83%D0%BC%D0%B0.user.js
 // ==/UserScript==
 
 (function () {
